700ca2eb0ec253083d35eb0c59ad0ac463828d6d
Pawel Chalkowski <chauek@gmail.com>
2020-03-20 21:52:48 +0100

    Fetching data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Papa from 'papaparse';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const confirmedUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+  getCsvData(confirmedUrl).then(confirmed => {
+    console.log('confirmed', confirmed);
+  });
+
+  const deathsUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv';
+  getCsvData(deathsUrl).then(deaths => {
+    console.log('deaths', deaths);
+  });
+
+  const recoveredUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv';
+  getCsvData(recoveredUrl).then(recovered => {
+    console.log('recovered', recovered);
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +39,26 @@ function App() {
       </header>
     </div>
   );
+}
+
+async function getCsvData(url: string) {
+  let csvData = await fetchCsv(url);
+
+  return Papa.parse(csvData, { header: true });
+}
+
+async function fetchCsv(url: string) {
+  return fetch(url).then(function (response) {
+    if (!response.body) {
+      return '';
+    }
+    let reader = response.body.getReader();
+    let decoder = new TextDecoder('utf-8');
+
+    return reader.read().then(function (result) {
+      return decoder.decode(result.value);
+    });
+  });
 }
 
 export default App;
